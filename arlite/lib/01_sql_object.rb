ace85c91bb81494d19b2b0a15573d506be64205d
DOV3D <davidovelazquez3@gmail.com>
2021-09-14 17:51:57 -0400

    Completed attr_accessor and began sql object

--- arlite/lib/01_sql_object.rb
+++ arlite/lib/01_sql_object.rb
@@ -6,6 +6,15 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    return @columns if @columns
+   @columns = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+       #{self.table_name}
+      LIMIT 1
+    SQL
+    .first.map(&:to_sym)
   end
 
   def self.finalize!
@@ -13,10 +22,12 @@ class SQLObject
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    @table_name || "#{self}".tableize
   end
 
   def self.all
